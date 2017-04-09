fd27f13d991138a1857df7eca5bde66cc8209a81
Chance <chance.eakin@gmail.com>
2017-04-09 08:33:22 -0500

    successfully incorporated react bootstrap into project

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import { Router, Route, hashHistory } from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Intro from './containers/Intro';
 import App from './containers/vr-app';
-
 
 ReactDOM.render((
   <Router history={hashHistory}>
-    <Route path="/" component={App}/>
+    <Route path="/" component={Intro}/>
+    <Route path="/vr" component={App}/>
   </Router>
 ), document.getElementById('app'));
